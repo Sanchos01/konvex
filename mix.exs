e194b1d01d7820767f5254aa48b26ef9a93a1a6d
Alexander Tuktarov <sanchos.1991@mail.ru>
2017-07-24 21:09:29 +0300

    rebase tinca

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Konvex.Mixfile do
   defp deps do
     [
       {:exactor, github: "sasa1977/exactor", tag: "0467f8100bc735405d597dbf94996195eb31e0b6", override: true},
-      {:tinca, github: "timCF/tinca"}
+      {:tinca, github: "Sanchos01/tinca"}
     ]
   end
 end
